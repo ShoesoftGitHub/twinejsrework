381342be4f2202f3bc6418baaff48200d9ae7e3e
Shoesoft <76711350+ShoesoftGitHub@users.noreply.github.com>
2021-08-17 15:17:38 +0200

    Update tag-label.tsx

--- src/components/tag/tag-label.tsx
+++ src/components/tag/tag-label.tsx
@@ -1,1 +1,22 @@
+import * as React from 'react';
+import {TagColors} from '../../store/stories';
+import './tag-label.css';
 
+export interface TagLabelProps {
+	tagColors: TagColors;
+	tags: string[];
+}
+
+export const TagLabel: React.FC<TagLabelProps> = React.memo(props => {
+	return (
+		<div className="tag-label">
+			{props.tags.map(tag => (
+				<span
+					className={`color-${props.tagColors[tag]}`}
+					key={tag}
+					title={tag}
+				/>
+			))}
+		</div>
+	);
+});
